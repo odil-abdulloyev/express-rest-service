43a0a2659db2d4e06e6e0f11b0a9620fad42db6d
Odil Abdulloyev <abdulloyevodil@mail.ru>
2021-05-28 00:32:12 +0500

    docs: add docs for board.service

--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,13 +1,37 @@
 const boardsRepo = require('./board.memory.repository');
 
+/**
+ * Get all boards
+ * @returns {Promise<Array<Board>>} - All boards
+ */
 const getAll = async () => boardsRepo.getAll();
 
+/**
+ * Create new board
+ * @param {Board} board - Board instance
+ * @returns {Promise<void>} - void
+ */
 const create = async (board) => boardsRepo.create(board);
 
+/**
+ * Get board by id
+ * @param {number|string} id - board id
+ * @returns {Promise<Board|undefined>} - board by id
+ */
 const getById = async (id) => boardsRepo.getById(id);
 
+/**
+ * Update board
+ * @param {Board} newBoard - new board
+ * @returns {Promise<boolean>} - true if updated, otherwise - false
+ */
 const update = async (newBoard) => boardsRepo.update(newBoard);
 
+/**
+ * Remove board by id
+ * @param {number|string} id - board id
+ * @returns {Promise<boolean>} - true if removed, otherwise - false
+ */
 const remove = async (id) => boardsRepo.remove(id);
 
 module.exports = { getAll, create, getById, update, remove };
