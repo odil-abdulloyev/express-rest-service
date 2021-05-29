3c4617c957a52cc04d34ce08b8565464d14e6c2e
Odil Abdulloyev <abdulloyevodil@mail.ru>
2021-05-29 22:45:44 +0500

    refactor: remove dashes in jsdoc description

--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -2,35 +2,35 @@ const boardsRepo = require('./board.memory.repository');
 
 /**
  * Get all boards
- * @returns {Promise<Array<Board>>} - All boards
+ * @returns {Promise<Array<Board>>} all boards
  */
 const getAll = async () => boardsRepo.getAll();
 
 /**
  * Create new board
- * @param {Board} board - Board instance
- * @returns {Promise<void>} - void
+ * @param {Board} board Board instance
+ * @returns {Promise<void>} void
  */
 const create = async (board) => boardsRepo.create(board);
 
 /**
  * Get board by id
- * @param {number|string} id - board id
- * @returns {Promise<Board|undefined>} - board by id
+ * @param {number|string} id board id
+ * @returns {Promise<Board|undefined>} board by id
  */
 const getById = async (id) => boardsRepo.getById(id);
 
 /**
  * Update board
- * @param {Board} newBoard - new board
- * @returns {Promise<boolean>} - true if updated, otherwise - false
+ * @param {Board} newBoard new board
+ * @returns {Promise<boolean>} true if updated, otherwise - false
  */
 const update = async (newBoard) => boardsRepo.update(newBoard);
 
 /**
  * Remove board by id
- * @param {number|string} id - board id
- * @returns {Promise<boolean>} - true if removed, otherwise - false
+ * @param {number|string} id board id
+ * @returns {Promise<boolean>} true if removed, otherwise - false
  */
 const remove = async (id) => boardsRepo.remove(id);
 
