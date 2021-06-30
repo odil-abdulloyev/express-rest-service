34372a1e94c19ee83810a28ede4edbbba5e83a7c
Odil Abdulloyev <abdulloyevodil@mail.ru>
2021-06-30 12:29:55 +0500

    feat: connect to postgres

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
+import 'reflect-metadata';
+import { ConnectionOptions, createConnection } from 'typeorm';
 import config from './common/config';
 import app from './app';
+import ormconfig from './ormconfig';
 
 const { PORT } = config;
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+createConnection(ormconfig as ConnectionOptions).then(() => {
+  app.listen(PORT, () =>
+    process.stdout.write(`App is running on http://localhost:${PORT}\n`)
+  );
+}).catch((error: Error) => process.stderr.write(error.message));
+
