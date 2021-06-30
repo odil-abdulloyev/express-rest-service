6d9eb0412b005a74904ff8183cb2c6fd0d812766
Odil Abdulloyev <abdulloyevodil@mail.ru>
2021-06-30 12:16:50 +0500

    fix: fix some express types

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction, RequestHandler } from 'express';
 import swaggerUI from 'swagger-ui-express';
 import path from 'path';
 import YAML from 'yamljs';
@@ -12,8 +12,8 @@ import uncaughtErrorHandler from './error-handlers/uncaught-error-handler';
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
-app.use(express.json());
-
+app.use(express.json() as RequestHandler);
+app.use(express.urlencoded({extended: true}) as RequestHandler);
 app.use(requestLogger);
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
