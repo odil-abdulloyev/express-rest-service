137c2caf2d26a1131c7e5139122f7c7e607b6cfd
Odil Abdulloyev <abdulloyevodil@mail.ru>
2021-07-04 13:15:46 +0500

    fix: check admin existance before create

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,15 @@ import app from './app';
 import ormconfig from './ormconfig';
 import * as userService from './resources/users/user.service';
 import IUser from './types/iuser';
+import User from './entity/user';
 
 const { PORT } = config;
 
 createConnection(ormconfig).then(async (connection) => {
   await connection.runMigrations();
-  await userService.create({name: 'Odil', login: 'admin', password: 'admin'} as IUser);
+  if (!await User.findOne({login: 'admin'})) {
+    await userService.create({name: 'Admin', login: 'admin', password: 'admin'} as IUser);
+  }
   app.listen(PORT, () =>
     process.stdout.write(`App is running on http://localhost:${PORT}\n`)
   );
